f6e99628093e009367c29b94d2bc66c282da92fc
Ben Parisot <me@benparisot.com>
2017-10-16 16:39:26 -0700

    add js functions that fade in story and populate it with user input

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,34 @@ $(document).ready(function() {
 	$("#blanks form").submit(function() {
 		$("#slide").slideUp(1200, function() {
 		});
-  event.preventDefault();
+
+		event.preventDefault();
 	});
+
+	$("#blanks form").submit(function() {
+		var inputName = $('input#name').val();
+		var inputAge = $('input#age').val();
+		var inputHeight = $('input#height').val();
+		var inputWeight = $('input#weight').val();
+
+		$('.name').text(inputName);
+		$('.age').text(inputAge);
+		$('.height').text(inputHeight);
+		$('.weight').text(inputeWeight);
+
+		event.preventDefault();
+	});
+
 
 	$("#submit").click(function() {
 		$('body').addClass('red-background');
+		$('.response').fadeTo(2500, 1, function () {
+			$('.next-response').fadeTo(1200, 1, function () {
+				$('.response-three').fadeTo(1200, 1, function () {
+					$('.response-four').fadeTo(1200, 1);
+				}););
+			}););
+		});
 	});
 
 
