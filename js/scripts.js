bfea5f25296b704a03629ac16a11052dac94ee04
Guest User <Guest@Epicodus-10C.local>
2017-10-16 19:29:50 -0700

    Complete project and prep to push to github

--- js/scripts.js
+++ js/scripts.js
@@ -10,22 +10,42 @@ $(document).ready(function() {
 	$("#blanks form").submit(function() {
 		var inputName = $('input#name').val();
 		var inputAge = $('input#age').val();
-		var inputHeight = $('input#height').val();
-		var inputWeight = $('input#weight').val();
+		var inputColor = $('input#color').val();
+		var inputFear = $('input#fear').val();
 
 		$('.name').text(inputName);
 		$('.age').text(inputAge);
-		$('.height').text(inputHeight);
-		$('.weight').text(inputeWeight);
+		$('.color').text(inputColor);
+		$('.fear').text(inputFear);
 
 		event.preventDefault();
 	});
 
 
 	$("#submit").click(function() {
-		$('body').addClass('red-background');
+		$('body').addClass('background1');
 		$('.response').fadeTo(2500, 1, function() {
-			$('.next-response').fadeTo(2500, 1);
+			$('.next-response').fadeTo(2500, 1, function() {
+				$('.response-three').fadeTo(3000, 1, function() {
+					$('.response-four').fadeTo(3000, 1, function() {
+						$('.response-five').fadeTo(3000, 1, function() {
+							$('.response-six').fadeTo(3000, 1, function() {
+								$('.response-seven').fadeTo(3000, 1, function() {
+									$('body').removeClass('background1').addClass('background2');
+									$('.response-eight').fadeTo(2500, 1, function () {
+										$('body').addClass('background3');
+										$('.response-nine').fadeTo(2500, 1, function() {
+											$('body').addClass('background4');
+											$('.response-ten').fadeTo(2500, 1);
+										});
+									});
+								});
+							});
+						});
+					});
+				});
+			});
+
 		});
 	});
 
